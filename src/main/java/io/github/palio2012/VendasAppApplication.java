c926305cbd3923a6a6524129cba3e65c24592cae
Palio2012 <daniasrayan.com@gmail.com>
2022-11-08 13:10:59 -0300

    H2 database connection established

---
+++ src/main/java/io/github/palio2012/VendasAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.palio2012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/palio2012/VendasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.palio2012;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
