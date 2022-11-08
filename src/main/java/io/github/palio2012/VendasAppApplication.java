4387e9d1b184ed810680a518ad104eb4e88b071e
Palio2012 <daniasrayan.com@gmail.com>
2022-11-08 15:13:51 -0300

    Insert and findAll methods implementation

--- src/main/java/io/github/palio2012/VendasAppApplication.java
+++ src/main/java/io/github/palio2012/VendasAppApplication.java
@@ -1,10 +1,29 @@
 package io.github.palio2012;
 
+import io.github.palio2012.domain.entities.Cliente;
+import io.github.palio2012.domain.respositorios.ClienteRep;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.List;
 
 @SpringBootApplication
 public class VendasAppApplication {
+
+    @Bean
+    public CommandLineRunner init (@Autowired ClienteRep clientes) {
+        return args -> {
+            clientes.salvar(new Cliente("Daniel"));
+            clientes.salvar(new Cliente("Roberto"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out :: println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasAppApplication.class, args);
---
+++ src/main/java/io/github/palio2012/domain/respositorios/ClienteRep.java
@@ -0,0 +1,37 @@
+package io.github.palio2012.domain.respositorios;
+
+import io.github.palio2012.domain.entities.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class ClienteRep {
+
+    private static String INSERT = "insert into cliente (nome) values (?)";
+    private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Cliente salvar(Cliente cliente) {
+        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+        return cliente;
+    }
+
+    public List <Cliente> obterTodos () {
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+            @Override
+            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+                Integer id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                return new Cliente("id, nome");
+            }
+        });
+    }
+}
